931a17582a5d1d1dba0c9a2635ded8357bd75ad6
Edward <edward6934@dingtalk.com>
2021-09-01 18:04:51 +0800

    update

--- android/pack.sh
+++ android/pack.sh
@@ -1,11 +1,11 @@
 #
 # CI 打包
-# 命令格式：./pack.sh debug Yuxiaor
+# ./pack.sh [release/debug] [Yuxiaor] [branch]
 #
 
-apiKey="6d040f4231bcda1dd5613cd27284f9bc"
-pgyer="https://www.pgyer.com/3sVc"
-imageKey="img_v2_2827ce84-1281-4036-8539-2b812cc9525g"
+api_key="6d040f4231bcda1dd5613cd27284f9bc"
+image_key="img_v2_2827ce84-1281-4036-8539-2b812cc9525g"
+href="https://www.pgyer.com/3sVc"
 
 # 默认参数
 type="debug"
@@ -21,57 +21,60 @@ if [[ -n $2 ]]; then
   flavor=$2
 fi
 
-if [ $type = "release" ]; then
-  buildType="Release"
-  apkPath="./app/build/outputs/apk/$flavor/$type"
-else
-  buildType="Debug"
-  apkPath="./app/build/outputs/apk/$flavor/$type"
+if [[ -n $3 ]]; then
+  branch=$3
+  git checkout $3
 fi
 
-# ----------------------------------------
+if [ $type = "release" ]; then
+  buildType="Release"
+else
+  buildType="Debug"
+fi
 
 # 打包
+git pull
 ./gradlew clean
 ./gradlew "assemble$flavor$buildType"
 
-# 查找APK
-files=$(ls $apkPath)
-for f in ${files}; do
-  if [ "${f:0-3:3}" = "apk" ]; then
-    file=$f
-    break
-  fi
-done
-apk="$apkPath/$file"
-
-if [ -z "$file" ]; then
-  echo
-  echo "打包失败！！！"
-  echo
-  read -r
+if [ $? -eq 0 ]; then
+  echo "打包成功!"
+else
+  echo "打包失败!"
   exit 1
 fi
 
+# 查找APK
+apks=()
+files=$(find . -name "*.apk")
+for f in ${files}; do
+  apks[${#apks[*]}]=$f
+done
+
+apk=${apks[0]}
+file_name=$(basename $apk)
+
 echo
-echo "打包成功"
-echo "$apk"
+if [ -z $apk ]; then
+  echo "APK Not Found!"
+  exit 1
+else
+  echo "APK info:"
+  echo "$branch"
+  echo "$apk"
+fi
 echo
 
 # 上传蒲公英
 echo
 echo "上传到蒲公英..."
 echo
-curl "https://www.pgyer.com/apiv2/app/upload" \
--F "_api_key=$apiKey" \
--F "file=@$apk"
+curl "https://www.pgyer.com/apiv2/app/upload" -F "_api_key=$api_key" -F "file=@$apk"
 
-if [ $flavor != "Yuxiaor" ]; then
-  echo
-  echo "只有打包寓小二会发送飞书消息，其他渠道不发送飞书消息。"
-  echo "若上传到蒲公英成功，则上面返回结果中的链接即为二维码链接。"
-  echo
-  read -r
+if [ $? -eq 0 ]; then
+  echo "上传成功!"
+else
+  echo "上传失败!"
   exit 1
 fi
 
@@ -79,35 +82,44 @@ fi
 echo
 echo "发送飞书消息..."
 echo
-text="[
-  {\"tag\": \"text\",\"text\": \"branch: $branch\"}
-]"
-
-img="[{
-\"tag\": \"img\",
-\"image_key\": \"$imageKey\",
-\"width\": 300,
-\"height\": 300
-}]"
-
-url="[{
-\"tag\": \"a\",
-\"text\": \"$file\",
-\"href\": \"$pgyer\"
-}]"
-
-raw="{
-  \"msg_type\": \"post\",
-  \"content\": {
-    \"post\": {
-        \"zh_cn\":{\"title\": \"Android APK\",\"content\": [$img,$url,$text]}
-    }
-  }
-}"
 
 curl "https://open.feishu.cn/open-apis/bot/v2/hook/13bad1ac-7482-4aa4-a744-a5db6ca119b1" \
 --header "Content-Type: application/json" \
---data-raw "$raw"
+--data-raw "
+{
+    \"msg_type\": \"post\",
+    \"content\": {
+        \"post\": {
+            \"zh_cn\": {
+                \"title\": \"Android APK\",
+                \"content\": [
+                    [
+                        {
+                            \"tag\": \"img\",
+                            \"image_key\": \"$image_key\",
+                            \"width\": 300,
+                            \"height\": 300
+                        }
+                    ],
+                    [
+                        {
+                            \"tag\": \"a\",
+                            \"text\": \"$file_name\",
+                            \"href\": \"$href\"
+                        }
+                    ],
+                    [
+                        {
+                            \"tag\": \"text\",
+                            \"text\": \"branch: $branch\"
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+}
+"
 
 # 结束
-read -r
+exit 0
